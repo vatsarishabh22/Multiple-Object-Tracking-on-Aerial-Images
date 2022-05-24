7499b80d1b46d96e32b1327ea957020eba97d4fd
Rishabh Vatsa <59773001+vatsarishabh22@users.noreply.github.com>
2022-05-24 12:06:34 +0530

    Update main_multiple.py

--- Detection less/main_multiple.py
+++ Detection less/main_multiple.py
@@ -1,35 +1,8 @@
 import os
 from os.path import exists, join, basename, splitext
-'''
-import ffmpeg as ffmpeg
-import rm as rm
-from pytube import YouTube
-
-from SiamMask.tools.test import siamese_init, siamese_track
-from SiamMask.utils.config_helper import load_config
-from SiamMask.utils.load_helper import load_pretrain
-'''
 
 git_repo_path = 'F:\ObjectTracking\SiamMask'
 project_name = splitext(basename(git_repo_path))[0]
-#if not exists(project_name):
-# clone and install dependencies
-''''!git
-    clone - q - -depth
-    1
-    {git_repo_url}
-    !sed - i
-    "/torch/d"
-    {project_name} / requirements.txt
-    !cd
-    {project_name} & & 
-    pip install - q - r requirements.txt
-    !cd
-    {project_name} & & bash
-    make.sh
-    !pip
-    install - q
-    #youtube - dl'''
 
 import sys
 
@@ -40,8 +13,6 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 plt.rcParams["axes.grid"] = False
-
-# from IPython.display import YouTubeVideo
 
 import cv2
 import torchvision
@@ -55,51 +26,21 @@ from types import SimpleNamespace
 from custom import Custom
 from tools.test import *
 
-#from types import SimpleNamespace
-#from SiamMask.experiments.siammask_base.custom import Custom
-#from SiamMask.tools.test import *
-
-''' code 2'''
-
 exp_path = join(project_name, 'experiments/siammask_sharp')
 pretrained_path1 = join(exp_path, 'SiamMask_DAVIS.pth')
 pretrained_path2 = join(exp_path, 'SiamMask_VOT.pth')
 
-# # download pretrained weights
-# if not exists(pretrained_path1):
-#     !cd {exp_path} && wget http://www.robots.ox.ac.uk/~qwang/SiamMask_DAVIS.pth
-# if not exists(pretrained_path2):
-#     !cd {exp_path} && wget http://www.robots.ox.ac.uk/~qwang/SiamMask_VOT.pth
-
-# init SiamMask
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 cfg = load_config(SimpleNamespace(config=join(exp_path, 'config_davis.json')))
 siammask = Custom(anchors=cfg['anchors'])
 siammask = load_pretrain(siammask, pretrained_path1)
 siammask = siammask.eval().to(device)
 
-''' code 3'''
-
-
-# YOUTUBE_ID = 'c_6-8kvLFCs'
-# YouTubeVideo(YOUTUBE_ID)
-
-# rm -rf youtube.mp4
-# # download the youtube with the given ID
-# !youtube -dl -f
-# 'bestvideo[ext=mp4][vcodec!*=av01]' - -output
-# "youtube.%(ext)s"
-# https://www.youtube.com/watch?v={YOUTUBE_ID}
-# yt = YouTube('https://www.youtube.com/watch?v=c_6-8kvLFCs').streams.first().download('E:\pythonProject1')
-
 f = 0
 video_capture = cv2.VideoCapture()
 if video_capture.open('input video.mp4'):
     width, height = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)), int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = video_capture.get(cv2.CAP_PROP_FPS)
-
-    # !rm -f output.mp4 output.avi
-    # # can't write out mp4, so try to write into an AVI file
     video_writer = cv2.VideoWriter("skating_output.avi", cv2.VideoWriter_fourcc(*'MJPG'), fps, (width, height))
 
     while video_capture.isOpened():
@@ -108,24 +49,6 @@ if video_capture.open('input video.mp4'):
             break
 
         if f == 0:
-            # detect a person on the first frame with Mask R-CNN
-            # maskrcnn = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
-            # maskrcnn = maskrcnn.eval()
-            # output = \
-            # maskrcnn([torchvision.transforms.functional.to_tensor((frame[..., ::-1] / 255).astype(np.float32))])[0]
-            #
-            # target_pos, target_sz = None, None
-            # for box, label, score in zip(output['boxes'], output['labels'], output['scores']):
-            #     # search for a person with atleast 70% probability
-            #     if score > 0.7 and label == 1:
-            #         box = box.numpy()
-            #         x, y = int(box[0]), int(box[1])
-            #         w, h = int(box[2]) - x, int(box[3]) - y
-            #         target_pos = np.array([x + w / 2, y + h / 2])
-            #         target_sz = np.array([w, h])
-            #         break
-            # assert target_pos is not None, "no person found on the first frame!"
-            # print("person found:", target_pos, target_sz)
             ROIs = []
             for i in range(3):
                 try:
@@ -137,11 +60,6 @@ if video_capture.open('input video.mp4'):
             targets = []
             for i in ROIs:
                 x, y, w, h = i
-                # init tracker
-                #im = cv2.imread("52855007_010_7773.jp")
-                # fromCenter = False
-                # r = cv2.selectROI("Image", frame, fromCenter)
-                # x,y,w,h=r
                 target_pos = np.array([x + w / 2, y + h / 2])
                 target_sz = np.array([w, h])
                 s = {"target_pos": target_pos, "target_sz": target_sz}
@@ -151,17 +69,7 @@ if video_capture.open('input video.mp4'):
             for target in targets:
                 state = siamese_init(frame, target['target_pos'], target['target_sz'], siammask, cfg['hp'], device=device)
                 states.append(state)
-
-            #state = siamese_init(frame, target_pos, target_sz, siammask, cfg['hp'], device=device)
         else:
-            # track
-            # state = siamese_track(state, frame, mask_enable=True, refine_enable=True, device=device)
-            # location = state['ploygon'].flatten()
-            # mask = state['mask'] > state['p'].seg_thr
-            #
-            # frame[:, :, 2] = (mask > 0) * 255 + (mask == 0) * frame[:, :, 2]
-            # cv2.polylines(frame, [np.int0(location).reshape((-1, 1, 2))], True, (0, 255, 0), 3)
-
             for i, state in enumerate(states):
 
                 state = siamese_track(state, frame, mask_enable=True, refine_enable=True, device=device)
@@ -180,8 +88,5 @@ if video_capture.open('input video.mp4'):
             break
     video_capture.release()
     video_writer.release()
-
-    # convert AVI to MP4
-    #!ffmpeg -y -loglevel info -i output.avi output.mp4
 else:
     print("can't open the given input video file!")
